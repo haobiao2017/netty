291446c2c2ab62ff0adefdee0e9e9b458cd2ea82
HaoBiao <1319295938@qq.com>
2019-10-04 18:55:50 +0800

    [study]初始化项目，一个简单的服务器和客户端

---
+++ src/main/java/com/hb/netty/TimeClient.java
@@ -0,0 +1,38 @@
+package com.hb.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TimeClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        new TimeClient().connect(8080, "127.0.0.1");
+    }
+
+    public void connect(int port, String host) throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+            ChannelFuture f = b.connect(host, port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+
+    }
+}
