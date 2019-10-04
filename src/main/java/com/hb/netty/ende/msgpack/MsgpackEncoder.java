c5e92c6f22b6a3d88cacd99c1ccb411061f12be8
HaoBiao <1319295938@qq.com>
2019-10-04 22:00:25 +0800

    [study]msgpack编码和解码

---
+++ src/main/java/com/hb/netty/ende/msgpack/MsgpackEncoder.java
@@ -0,0 +1,15 @@
+package com.hb.netty.ende.msgpack;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import org.msgpack.MessagePack;
+
+public class MsgpackEncoder extends MessageToByteEncoder<Object> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception {
+        MessagePack pack = new MessagePack();
+        byte[] bytes = pack.write(msg);
+        out.writeBytes(bytes);
+    }
+}
