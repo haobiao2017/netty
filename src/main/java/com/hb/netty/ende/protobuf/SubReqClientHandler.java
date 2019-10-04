8fd1415d1bc6d225e7d4b6204ee5c5a57a5e00a5
HaoBiao <1319295938@qq.com>
2019-10-05 00:06:13 +0800

    [study]protobuf服务器客户端使用

---
+++ src/main/java/com/hb/netty/ende/protobuf/SubReqClientHandler.java
@@ -0,0 +1,39 @@
+package com.hb.netty.ende.protobuf;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class SubReqClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            ctx.write(subReq(i));
+        }
+        ctx.flush();
+    }
+
+    private SubscribeReqProto.SubscribeReq subReq(int i) {
+        SubscribeReqProto.SubscribeReq.Builder builder = SubscribeReqProto.SubscribeReq.newBuilder();
+        builder.setSubReqID(i);
+        builder.setUserName("HaoBiao");
+        builder.setProductName("Netty Book For Protobuf");
+        builder.setAddress("Chengdu");
+        return builder.build();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Receive server response:[" + msg + "]");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
